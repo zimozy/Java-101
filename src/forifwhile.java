f2e665929560df5986b93738c4c0c2db80d0f466
Tim <eafwas@gmail.com>
2017-08-18 19:03:57 -0400

    test

--- src/forifwhile.java
+++ src/forifwhile.java
@@ -4,25 +4,26 @@
  * are Fibonnaci numbers. A Fibonacci number is equal to the sum of the previous two
  * numbers in the Fibonacci sequence:
  * 1 1 2 3 5 8 13 and so on...
+ * I made a change here .......
  */
 
 public class forifwhile {
-	
+
 	public static void main(String[] args) {
 		int fibs[]; // Holds our Fibonacci numbers
 		int placeHolder; // This is a placeHolderholder for the most recently discovered Fibonacci number
 		int max; // max number of fibbonaci numbers calculated before stopping
 		int counter; // this will have the value of every integer until we stop
-				
+
 		counter = 0; // start at zero
 		max = 10; // max number of Fibonacci numbers calculated
-		
+
 		fibs = new int[max]; // Fibonacci array
-		
+
 		fibs[0] = 1; // we set up the first two items in the sequence to get things going
 		fibs[1] = 1;
 		placeHolder = 2; // the placeHolderholder starts at 2 because we have already set the first two values
-		
+
 		/* WHILE loop */
 		while (placeHolder < max) { //loop until we reach our maximum limit
 			/* IF statement */
@@ -33,7 +34,7 @@ public class forifwhile {
 			}
 			counter++; // advance the counter
 		}
-		
+
 		/* FOR loop */
 		System.out.println("The first five Fibonacci numbers are:");
 		for (int i = 0; i < 5; i++) { // Let's loop five times
